9c520a186cbf1e76d4a40940e26e5cf749fe72fc
sashabuzin <n9159648714@gmail.com>
2021-05-29 14:57:09 +0300

    lesson1 is finished

--- lesson1/app/src/main/java/com/buzinasgeekbrains/lesson1/MainActivity.java
+++ lesson1/app/src/main/java/com/buzinasgeekbrains/lesson1/MainActivity.java
@@ -2,13 +2,39 @@ package com.buzinasgeekbrains.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnActTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                Intent intent = new Intent(this, ActivityTwo.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
