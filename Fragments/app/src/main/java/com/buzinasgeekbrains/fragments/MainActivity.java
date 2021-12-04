26551e3eb1bce4af8d13aab20ad0947c5ec51f8b
sashabuzin <n9159648714@gmail.com>
2021-12-04 21:32:56 +0300

    Lesson 8 is finished.

--- Fragments/app/src/main/java/com/buzinasgeekbrains/fragments/MainActivity.java
+++ Fragments/app/src/main/java/com/buzinasgeekbrains/fragments/MainActivity.java
@@ -1,14 +1,18 @@
 package com.buzinasgeekbrains.fragments;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.res.Configuration;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 //          Задание:
 //        1. Создайте боковое навигационное меню для своего приложения и добавьте туда хотя бы
 //        один экран, например «Настройки» или «О приложении».
@@ -32,6 +36,35 @@ public class MainActivity extends AppCompatActivity {
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_draver_open,
+                R.string.navigation_draver_close
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.action_about) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .addToBackStack("")
+                        .replace(R.id.fragment_container_1, new AboutFragment()).commit();
+                drawerLayout.closeDrawers();
+                return true;
+            } else if (id == R.id.action_exit)  {
+                finish();
+                return true;
+            }
+            return false;
+        });
     }
 
     @Override
